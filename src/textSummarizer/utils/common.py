cbc1c1496961ab462aec13e28db00d5c6c12a410
Abubakar Saddiq <abubakarsaddiq0011992gmail.com>
2024-12-03 18:18:42 +0500

    all setups

--- src/textSummarizer/utils/common.py
+++ src/textSummarizer/utils/common.py
@@ -38,12 +38,14 @@ def create_directories(path_to_directories: list, verbose=True):
 
     Args:
         path_to_directories (list): list of path of directories
-        ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
+        ignore_log (bool, optional): ignore if multiple dirs is to be created.
+        Defaults to False.
     """
     for path in path_to_directories:
         os.makedirs(path, exist_ok=True)
         if verbose:
             logger.info(f"created directory at: {path}")
+
 
 @ensure_annotations
 def get_size(path: Path) -> str:
